8a0ad349f0d2dcabfda6d53f5212d6eeeb03ff31
Logan2134 <Logan.gill2578@gmail.com>
2023-02-11 12:16:31 -0500

    Added and tested rendering system

--- GameEngine_LGJ/Main.cpp
+++ GameEngine_LGJ/Main.cpp
@@ -7,6 +7,28 @@ int main(int argc, char* args[])
 	Engine& GameEngine = Engine::GetInstance();
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Game Engine");
+
+	// create world so we can add entities
+	GameEngine.world = ECS::World::createWorld();
+
+	//create our first entity
+	ECS::Entity* myEnt;
+
+	//add systems to the engine
+	GameEngine.AddSystem(new RenderingSystem());
+
+	//create 250 entites to the world
+	for (int i = 0; i < 250; i++)
+	{
+		for (int j = 0; j < 1; j++)
+		{
+			myEnt = GameEngine.world->create();
+			myEnt->assign<Transform>(i * 250, j * 32);
+			myEnt->assign<Sprite2D>("../Debug/Pictures/hero.png");
+			std::cout << "id: " << myEnt->getEntityId() << std::endl;
+		}
+	}
+
 	//pass window refernce to engine and start
 	GameEngine.Start(&window);
 
