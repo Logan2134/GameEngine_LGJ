4bd357290f05b4acf95b62ae60aaec6e58981e87
Logan2134 <Logan.gill2578@gmail.com>
2023-03-04 12:14:47 -0500

    added input and movement systems

---
+++ GameEngine_LGJ/Systems/MovementSystem.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Core/Engine.h"
+class MovementSystem : public ECS::EntitySystem
+{
+public :
+	MovementSystem() = default;
+	
+	~MovementSystem() = default;
+	
+	void tick(ECS::World* world, float deltaTime)override;
+
+};
+
